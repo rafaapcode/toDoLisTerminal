1a90a3638be5b4902bb08dc853c79ea7fd00df3c
Rafael Aparecido <rafaap2003@gmail.com>
2022-12-05 21:35:24 -0300

    test: ensure Create method calls with correct values

--- src/utils/createTable/createTable.spec.ts
+++ src/utils/createTable/createTable.spec.ts
@@ -14,4 +14,16 @@ describe('Create Table', () => {
     const result2 = sut.create(tasks)
     expect(result2).toBe(4)
   })
+
+  test('Should create method calls correct values', () => {
+    const sut = new Table()
+    const spyTable = jest.spyOn(sut, 'create')
+    const tasks = [
+      { id: 1, task: 'Estudar inglês' },
+      { id: 2, task: 'Estudar inglês' },
+      { id: 3, task: 'Estudar inglês' }
+    ]
+    sut.create(tasks)
+    expect(spyTable).toHaveBeenCalledWith(tasks)
+  })
 })
