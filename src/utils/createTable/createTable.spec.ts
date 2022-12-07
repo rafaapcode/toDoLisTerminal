fec1df767218621081e7e5eec9fea03077415fb8
Rafael Aparecido <rafaap2003@gmail.com>
2022-12-07 16:31:15 -0300

    refactor: create a helper method to make a FakeTest

--- src/utils/createTable/createTable.spec.ts
+++ src/utils/createTable/createTable.spec.ts
@@ -1,4 +1,13 @@
 import Table from './createTable'
+import { TaskModel } from '../../domain/usecases/addtask/addtask'
+
+const makeFakeTask = (): TaskModel[] => {
+  return [
+    { id: 1, body: 'Estudar inglês' },
+    { id: 2, body: 'Estudar inglês' },
+    { id: 3, body: 'Estudar inglês' }
+  ]
+}
 
 const makeSut = (): Table => {
   return new Table()
@@ -7,27 +16,14 @@ const makeSut = (): Table => {
 describe('Create Table', () => {
   test('Should return correct quantity of tasks', () => {
     const sut = makeSut()
-    const tasks = [
-      { id: 1, body: 'Estudar inglês' },
-      { id: 2, body: 'Estudar inglês' },
-      { id: 3, body: 'Estudar inglês' }
-    ]
-    const result = sut.create(tasks)
+    const result = sut.create(makeFakeTask())
     expect(result).toBe(3)
-    tasks.push({ id: 4, body: 'Estudar inglês' })
-    const result2 = sut.create(tasks)
-    expect(result2).toBe(4)
   })
 
   test('Should create method calls correct values', () => {
     const sut = makeSut()
     const spyTable = jest.spyOn(sut, 'create')
-    const tasks = [
-      { id: 1, body: 'Estudar inglês' },
-      { id: 2, body: 'Estudar inglês' },
-      { id: 3, body: 'Estudar inglês' }
-    ]
-    sut.create(tasks)
-    expect(spyTable).toHaveBeenCalledWith(tasks)
+    sut.create(makeFakeTask())
+    expect(spyTable).toHaveBeenCalledWith(makeFakeTask())
   })
 })
